b9425eb40791d33cc4e99f7729af01129adbb403
Michał Kopacz <kopaczmichal093@gmail.com>
2020-10-07 16:56:21 +0200

    Add profile and settings subpages

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
-router.get('/logged', (req, res) => {
-	res.render('logged');
+const loggedUser = (req, res, next) => {
+	if (!req.user) res.redirect('/user/no-permission');
+	else next();
+}
+
+router.get('/logged', loggedUser, (req, res) => {
+		res.render('logged', { name: req.user.displayName, photo: req.user.photos[0].value});
 });
+
+router.get('/profile', loggedUser, (req, res)=> {
+		res.render('profile', { name: req.user.displayName });
+});
+
+router.get('/profile/settings', loggedUser, (req, res)=> {
+		res.render('settings');
+	});
 
 router.get('/no-permission', (req, res) => {
 	res.render('noPermission');
