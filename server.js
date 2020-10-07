f0892f72672bad823b4268adbc639ca2530cfaf8
Michał Kopacz <kopaczmichal093@gmail.com>
2020-10-07 11:26:30 +0200

    Add passport config

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const session = require('express-session');
+
+// Passport config
+passport.use(new GoogleStrategy({
+  clientID: '711438492722-m605ebn1i9139vvha8to3pv19st2h92o.apps.googleusercontent.com',
+  clientSecret: 'gn6h9GL2a_f25VDXS23CJcD2',
+  callbackURL: 'http://localhost:8000/auth/callback'
+},
+  (accessToken, refreshToken, profile, done) => {
+  done(null, profile);
+}));
 
 const app = express();
 
